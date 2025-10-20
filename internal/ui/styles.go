7d771f63710ea4700581f8e2dba925648956fee0
owenhochwald <owenhochwald@gmail.com>
2025-10-19 21:40:45 -0700

    add sidebar and main panel layouts

--- internal/ui/styles.go
+++ internal/ui/styles.go
@@ -1,1 +1,13 @@
 package ui
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	SidebarStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			Width(20).
+			BorderForeground(lipgloss.Color("62"))
+	MainStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("63"))
+)
