201c0a783afd928f13c1cdc12e7cca7d06b419a8
owenhochwald <owenhochwald@gmail.com>
2025-10-20 00:12:52 -0700

    create request sqlite type def

---
+++ internal/http/request.go
@@ -0,0 +1,10 @@
+package http
+
+type Request struct {
+	ID      string            `json:"id"`
+	Name    string            `json:"name"`
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body"`
+}
