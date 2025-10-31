c8ef38516eb46be03d04d4944b6d6b91b3edc75f
owenhochwald <owenhochwald@gmail.com>
2025-10-31 13:45:53 -0700

    add const methods and request constructor

--- internal/http/request.go
+++ internal/http/request.go
@@ -1,5 +1,17 @@
 package http
 
+const (
+	GET     = "GET"
+	HEAD    = "HEAD"
+	POST    = "POST"
+	PUT     = "PUT"
+	DELETE  = "DELETE"
+	CONNECT = "CONNECT"
+	PATCH   = "PATCH"
+	OPTIONS = "OPTIONS"
+	TRACE   = "TRACE"
+)
+
 type Request struct {
 	ID      string            `json:"id,omitempty"`
 	Name    string            `json:"name,omitempty"`
@@ -7,4 +19,18 @@ type Request struct {
 	URL     string            `json:"url"`
 	Headers map[string]string `json:"headers,omitempty"`
 	Body    string            `json:"body,omitempty"`
+}
+
+func NewRequest() *Request {
+	return &Request{
+		Method: "",
+	}
+}
+
+func NewRequestWithParams(method, url string) *Request {
+	return &Request{
+		Method:  method,
+		URL:     url,
+		Headers: make(map[string]string),
+	}
 }
