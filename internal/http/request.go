59f144474ca2db088910c750dbee0edb6af9303e
owenhochwald <owenhochwald@gmail.com>
2025-10-31 17:09:15 -0700

    add request validation and test methods

--- internal/http/request.go
+++ internal/http/request.go
@@ -1,5 +1,10 @@
 package http
 
+import (
+	"fmt"
+	"slices"
+)
+
 const (
 	GET     = "GET"
 	HEAD    = "HEAD"
@@ -12,6 +17,18 @@ const (
 	TRACE   = "TRACE"
 )
 
+var validMethods = []string{
+	GET,
+	HEAD,
+	POST,
+	PUT,
+	DELETE,
+	CONNECT,
+	PATCH,
+	OPTIONS,
+	TRACE,
+}
+
 type Request struct {
 	ID      string            `json:"id,omitempty"`
 	Name    string            `json:"name,omitempty"`
@@ -21,9 +38,19 @@ type Request struct {
 	Body    string            `json:"body,omitempty"`
 }
 
-func NewRequest() *Request {
+func NewBlankRequest() *Request {
 	return &Request{
 		Method: "",
+	}
+}
+
+func NewDefaultRequest() *Request {
+	return &Request{
+		Name:    "None",
+		Method:  GET,
+		URL:     "http://localhost:",
+		Headers: make(map[string]string),
+		Body:    "",
 	}
 }
 
@@ -33,4 +60,42 @@ func NewRequestWithParams(method, url string) *Request {
 		URL:     url,
 		Headers: make(map[string]string),
 	}
+}
+
+func (r *Request) Validate() error {
+	if r.ID != "" && len(r.ID) > 10 {
+		return fmt.Errorf("id too long: %s", r.ID)
+	}
+
+	if r.Name != "" && len(r.Name) > 40 {
+		return fmt.Errorf("name too long: %s", r.Name)
+	}
+	if r.Method == "" {
+		return fmt.Errorf("method is required")
+	}
+	if r.URL == "" {
+		return fmt.Errorf("url is required")
+	}
+
+	if !slices.Contains(validMethods, r.Method) {
+		return fmt.Errorf("invalid method: %s", r.Method)
+	}
+
+	if r.URL != "" {
+		if len(r.URL) > 300 {
+			return fmt.Errorf("url too long: %s", r.URL)
+		}
+		if r.URL[0:4] != "http" {
+			return fmt.Errorf("invalid url: %s", r.URL)
+		}
+	}
+
+	if r.Headers != nil && len(r.Headers) > 100 {
+		return fmt.Errorf("too many headers: %d", len(r.Headers))
+	}
+	if r.Body != "" && len(r.Body) > 10000 {
+		return fmt.Errorf("body too long: %d", len(r.Body))
+	}
+
+	return nil
 }
