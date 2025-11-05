d0df55f87f5caff0845873524153028b0b07a2ed
owenhochwald <owenhochwald@gmail.com>
2025-11-04 16:45:41 -0800

    refactor types on response struct

--- internal/http/response.go
+++ internal/http/response.go
@@ -1,10 +1,12 @@
 package http
 
+import "net/http"
+
 type Response struct {
-	StatusCode int               `json:"status_code"`
-	Status     string            `json:"status,omitempty"`
-	Headers    map[string]string `json:"headers,omitempty"`
-	Body       string            `json:"body,omitempty"`
-	Duration   int64             `json:"duration,omitempty"`
-	Error      string            `json:"error,omitempty"`
+	StatusCode int         `json:"status_code"`
+	Status     string      `json:"status,omitempty"`
+	Headers    http.Header `json:"headers,omitempty"`
+	Body       string      `json:"body,omitempty"`
+	Duration   int64       `json:"duration,omitempty"`
+	Error      string      `json:"error,omitempty"`
 }
