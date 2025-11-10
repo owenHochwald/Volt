c7da58fe24a2786b9108fa3d67ab4a32143c82e5
owenhochwald <owenhochwald@gmail.com>
2025-11-09 23:58:41 -0800

    add basic viewport styling

--- internal/ui/response.go
+++ internal/ui/response.go
@@ -1,8 +1,11 @@
 package ui
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/owenHochwald/volt/internal/http"
 	"github.com/owenHochwald/volt/internal/utils"
 )
@@ -37,11 +40,29 @@ func (m *ResponsePane) GetCurrentMethod() string {
 	return ""
 }
 
+func (m ResponsePane) renderHeaderBar() string {
+	statusStyle := utils.MapStatusCodeToColor(m.Response.StatusCode)
+	status := statusStyle.Render(m.Response.Status)
+	duration := fmt.Sprintf(" %d ms", m.Response.Duration)
+	size := fmt.Sprintf(" %d bytes", len([]byte(m.Response.Body)))
+
+	return lipgloss.JoinHorizontal(lipgloss.Left, " | ", status, " | ", duration, " | ", size)
+}
+
 func (m ResponsePane) View() string {
 	if m.Response == nil {
 		return "Make a request to see the response here!"
 	}
-	return m.viewport.View() + utils.MapStatusCodeToColor(m.Response.StatusCode).Render(m.Response.Status)
+
+	// add in response header bar for error routes
+	if m.Response.Error != "" {
+		m.viewport.SetContent(m.Response.Error)
+		lipgloss.NewStyle().Foreground(lipgloss.Color("160")).Background(lipgloss.Color("160")).Render(
+			m.viewport.View(),
+		)
+	}
+	statusBar := m.renderHeaderBar()
+	return lipgloss.JoinVertical(lipgloss.Left, statusBar, m.viewport.View())
 }
 
 func (m ResponsePane) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
