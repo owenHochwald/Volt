0c22e6483e482ccadb445eb8f70de4b204158951
owenhochwald <owenhochwald@gmail.com>
2025-11-09 23:09:00 -0800

    add initial response struct with viewport rendering

--- internal/ui/response.go
+++ internal/ui/response.go
@@ -1,12 +1,17 @@
 package ui
 
 import (
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/owenHochwald/volt/internal/http"
 )
 
 type ResponsePane struct {
-	Response *http.Response
+	Response      *http.Response
+	height, width int
+	ready         bool
+
+	viewport viewport.Model
 }
 
 func (m ResponsePane) Init() tea.Cmd {
@@ -17,6 +22,11 @@ func (m *ResponsePane) SetFocused(focused bool) {
 }
 
 func (m *ResponsePane) SetHeight(height int) {
+	m.height = height
+}
+
+func (m *ResponsePane) SetWidth(width int) {
+	m.width = width
 }
 
 func (m *ResponsePane) GetCurrentMethod() string {
@@ -24,13 +34,41 @@ func (m *ResponsePane) GetCurrentMethod() string {
 }
 
 func (m ResponsePane) View() string {
-	return ""
+	if m.Response == nil {
+		return "Make a request to see the response here!"
+	}
+	//if !m.ready {
+	//	return "\n  Initializing..."
+	//}
+	return m.viewport.View() + m.Response.Status
 }
 
 func (m ResponsePane) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		if !m.ready {
+			m.viewport = viewport.New(msg.Width, msg.Height)
+			m.viewport.SetContent(m.Response.Body)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height
+		}
+	}
+
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func SetupResponsePane() ResponsePane {
-	return ResponsePane{}
+	return ResponsePane{
+		viewport: viewport.Model{},
+	}
 }
