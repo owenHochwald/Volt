a9c91ab2bf55f49544d16fc346c05be8667b325c
owenhochwald <owenhochwald@gmail.com>
2025-11-29 11:32:21 -0800

    add `GetAllURLs` method to SQLiteStorage with unit tests

--- internal/storage/sqlite_storage_test.go
+++ internal/storage/sqlite_storage_test.go
@@ -113,3 +113,87 @@ func TestSQLiteStorage_MultipleRequests(t *testing.T) {
 		assert.SliceContains(t, loaded, *req)
 	}
 }
+
+func TestSQLiteStorage_GetAllURLs_MultipleURLs(t *testing.T) {
+	db := setupTestDB(t)
+	numUniqueRequests := 4
+
+	requests := []*http.Request{
+		{
+			Name:   "test1",
+			Method: "GET",
+			URL:    "http://localhost:8080",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: "test",
+		},
+		{
+			Name:   "Not a distinct URL",
+			Method: "GET",
+			URL:    "http://localhost:8080",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: "test",
+		},
+		{
+			Name:   "Mostly the same URL, but still distinct",
+			Method: "GET",
+			URL:    "http://localhost:9090/test",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: "test",
+		},
+		{
+			Name:    "test2",
+			Method:  "POST",
+			URL:     "http://google.com/api",
+			Headers: map[string]string{},
+		},
+		{
+			Name:    "test2",
+			Method:  "POST",
+			URL:     "http://google.com/api",
+			Headers: map[string]string{},
+		},
+		{
+			Name:    "test3",
+			Method:  "PUT",
+			URL:     "http://broken;??/asd",
+			Headers: map[string]string{},
+		},
+	}
+
+	for _, req := range requests {
+		if err := db.Save(req); err != nil {
+			t.Fatalf("failed to save request: %v", err)
+		}
+	}
+
+	urls, err := db.GetAllURLs()
+
+	assert.Equal(t, numUniqueRequests, len(urls))
+	assert.NoError(t, err)
+	for _, req := range requests {
+		assert.SliceContains(t, urls, req.URL)
+	}
+}
+
+func TestSQLiteStorage_GetAllURLs_NoURLs(t *testing.T) {
+	db := setupTestDB(t)
+
+	requests := []*http.Request{}
+
+	for _, req := range requests {
+		if err := db.Save(req); err != nil {
+			t.Fatalf("failed to save request: %v", err)
+		}
+	}
+
+	urls, err := db.GetAllURLs()
+
+	assert.Equal(t, len(requests), len(urls))
+	assert.NoError(t, err)
+}
