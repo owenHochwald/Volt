3c71c83c8e115e3b07bd7fece07c47b3b9ed86f2
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-07-25 12:00:19 -0400

    refactor: inject response pane styles

--- internal/ui/responsepane/setup.go
+++ internal/ui/responsepane/setup.go
@@ -2,11 +2,17 @@ package responsepane
 
 import (
 	"charm.land/bubbles/v2/viewport"
+	"github.com/owenHochwald/Volt/internal/ui/design"
 	"github.com/owenHochwald/Volt/internal/ui/keybindings"
 )
 
 // SetupResponsePane creates and initializes a new ResponsePane with default values
-func SetupResponsePane(keys keybindings.KeyMap) ResponsePane {
+func SetupResponsePane(keys keybindings.KeyMap, optionalStyles ...design.Styles) ResponsePane {
+	styles := design.NewStyles(design.DefaultTheme())
+	if len(optionalStyles) > 0 {
+		styles = optionalStyles[0]
+	}
+
 	return ResponsePane{
 		viewport:   viewport.New(viewport.WithWidth(20), viewport.WithHeight(10)),
 		width:      20,
@@ -14,5 +20,6 @@ func SetupResponsePane(keys keybindings.KeyMap) ResponsePane {
 		activeTab:  int(TabBody), // Start on Body tab
 		isLoadTest: false,
 		keys:       keys,
+		styles:     styles,
 	}
 }
