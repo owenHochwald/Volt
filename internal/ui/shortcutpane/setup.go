7c5f5251b392eea71156150a321141e07b56e95f
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-07-25 12:05:01 -0400

    refactor: inject shortcut pane styles

--- internal/ui/shortcutpane/setup.go
+++ internal/ui/shortcutpane/setup.go
@@ -1,9 +1,17 @@
 package shortcutpane
 
-import "github.com/owenHochwald/Volt/internal/ui/keybindings"
+import (
+	"github.com/owenHochwald/Volt/internal/ui/design"
+	"github.com/owenHochwald/Volt/internal/ui/keybindings"
+)
 
 // SetupShortcutPane sets up the shortcut pane for use
-func SetupShortcutPane(keys keybindings.KeyMap) ShortcutPane {
+func SetupShortcutPane(keys keybindings.KeyMap, optionalStyles ...design.Styles) ShortcutPane {
+	styles := design.NewStyles(design.DefaultTheme())
+	if len(optionalStyles) > 0 {
+		styles = optionalStyles[0]
+	}
+
 	return ShortcutPane{
 		activeTab: 0,
 		height:    30,
@@ -11,6 +19,7 @@ func SetupShortcutPane(keys keybindings.KeyMap) ShortcutPane {
 		Focused:   false,
 		keys:      keys,
 		tabs:      GetShortcutTabs(keys),
+		styles:    styles,
 	}
 
 }
