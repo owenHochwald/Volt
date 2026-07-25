4bb1feb8f6742359b558f62fbee81c9869a46814
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-07-25 11:52:51 -0400

    refactor: centralize response status colors

--- internal/utils/statusCodeColorMapper.go
+++ internal/utils/statusCodeColorMapper.go
@@ -1,33 +1,23 @@
 package utils
 
 import (
-	"strconv"
-
 	"charm.land/lipgloss/v2"
+	"github.com/owenHochwald/Volt/internal/ui/design"
 )
 
-var (
-	successStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("76"))
-	permErrorStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("208"))
-	serverErrorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("160"))
-	weirdErrorStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("96"))
-)
+var statusStyles = design.NewStyles(design.DefaultTheme()).Status
 
 func MapStatusCodeToColor(statusCode int) lipgloss.Style {
-	statusString := strconv.Itoa(statusCode)
-	if len(statusString) != 3 {
-		return weirdErrorStyle
-	}
-
-	switch statusString[0] {
-	case '2':
-		return successStyle
-	case '3':
-		return permErrorStyle
-	case '4', '5':
-		return serverErrorStyle
+	switch statusCode / 100 {
+	case 2:
+		return statusStyles.Success
+	case 3:
+		return statusStyles.Redirect
+	case 4:
+		return statusStyles.ClientError
+	case 5:
+		return statusStyles.ServerError
 	default:
-		return weirdErrorStyle
+		return statusStyles.Unknown
 	}
-
 }
